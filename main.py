306d7d2214f9e34deedfe63b2febbe55cbb1ebd1
Айганым Сагидоллинова <58025616+aiganym2012@users.noreply.github.com>
2023-02-08 12:01:39 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
-# 🚨 Don't change the code below 👇
-print("Welcome to the Love Calculator!")
-name1 = input("What is your name? \n")
-name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-name11= name1.lower() 
-name22= name2.lower() 
-true = name11.count("t") + name11.count("r") + name11.count("u") + name11.count("e") + name22.count("t") + name22.count("r") + name22.count("u") + name22.count("e")
-love = name11.count("l") + name11.count("o") + name11.count("v") + name11.count("e") + name22.count("l") + name22.count("o") + name22.count("v") + name22.count("e")
-z = int(str(true) + str(love))
-print(z)
 
-if z<10  or z>90:
-  print(f"Your score is {z}, you go together like coke and mentos.")
-elif z>40 and z<50:
-  print(f"Your score is {z}, you are alright together.")
+
+first = input("You re at a crossroad. Where do you want to go? Type left or right")
+if first=="left":
+        sec = input("You've come to a lake.  There is an island in the middle of the lake. Type wait to wait for a boat. Type swim to swim across.")
+        if sec=="swim":
+              third= input("You arrive at the island unharmed. There is a house with 3 doors. One red,                                 one yellow and one blue. Which colour do you choose?")
+              if third=="red":
+                print("It's a room full of fire. Game Over.")
+              elif third=="blue":
+                print("You found the treasure! You Win!")
+              else:
+                  print("You enter a room of beasts. Game Over.")
+        else:
+              print("You get attacked by an angry trout. Game Over.")
+
 else:
-  print(f"Your score is {z}.")
-
+    print("You fell into a hole. Game Over.")
