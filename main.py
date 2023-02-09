1b4225951ec17d726df84d8e50fab7a34d5a380c
Айганым Сагидоллинова <58025616+aiganym2012@users.noreply.github.com>
2023-02-09 11:47:47 +0000

    it randomly choos who will pay the bill

--- main.py
+++ main.py
@@ -1,46 +1,12 @@
-print('''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
-|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+# Import the random module here
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
-
-first = input("You re at a crossroad. Where do you want to go? Type left or right")
-if first=="left":
-        sec = input("You've come to a lake.  There is an island in the middle of the lake. Type wait to wait for a boat. Type swim to swim across.")
-        if sec=="swim":
-              third= input("You arrive at the island unharmed. There is a house with 3 doors. One red,                                 one yellow and one blue. Which colour do you choose?")
-              if third=="red":
-                print("It's a room full of fire. Game Over.")
-              elif third=="blue":
-                print("You found the treasure! You Win!")
-              else:
-                  print("You enter a room of beasts. Game Over.")
-        else:
-              print("You get attacked by an angry trout. Game Over.")
-
-else:
-    print("You fell into a hole. Game Over.")
+print(names)
+sum=random.randint(0, len(names))
+who=names[sum]
+print(f"{who} is going to buy the meal today!" )
